0b17abdbfaaaae7102235a26338cc914534bc1c7
Boobs <86918103+braboobssiere@users.noreply.github.com>
2026-06-30 12:27:22 +0700

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from 'next';
 import { Analytics } from '@vercel/analytics/next';
 import { SpeedInsights } from '@vercel/speed-insights/next';
+import './globals.css';
 
 export const metadata: Metadata = {
   title: 'Private Article Reader',
